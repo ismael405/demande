0af25a6449d8f827f75bcc77c45f7bcbcbb800dc
ismael405 <andriabam36@gmail.com>
2026-02-04 19:46:31 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function ouvrirCoeur(){
+    document.getElementById("message").style.display = "block";
+    document.getElementById("music").play();
+}
+
+function reponse(){
+    alert("Tu viens de rendre Abraham le plus heureux du monde ❤️");
+}
